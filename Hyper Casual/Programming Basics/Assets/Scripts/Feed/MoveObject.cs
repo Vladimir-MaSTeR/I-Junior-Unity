72fbad49fe754028d44fafffc6e36f02ec739036
Vladimir Vladimirovich <58000132+Vladimir-MaSTeR@users.noreply.github.com>
2021-01-14 22:39:09 +0700

    Add files via upload

---
+++ Hyper Casual/Programming Basics/Assets/Scripts/Feed/MoveObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+
+    [SerializeField] private float _speed = 10;
+
+    private void OnCollisionStay(Collision collision) {
+        collision.rigidbody.AddForce(Vector3.left * _speed);
+    }
+
+}
